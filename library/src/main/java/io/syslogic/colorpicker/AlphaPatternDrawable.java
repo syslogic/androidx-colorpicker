af6825b5b2e0888ae95dc0942ce7c6e8b254ae6b
Martin Zeitler <martin@syslogic.io>
2022-02-28 00:51:03 +0100

    gradle task changed.

--- library/src/main/java/io/syslogic/colorpicker/AlphaPatternDrawable.java
+++ library/src/main/java/io/syslogic/colorpicker/AlphaPatternDrawable.java
@@ -20,11 +20,10 @@ import androidx.annotation.Nullable;
  */
 public class AlphaPatternDrawable extends Drawable {
 
-    private int mRectangleSize = 10;
-
-    private Paint mPaint = new Paint();
-    private Paint mPaintWhite = new Paint();
-    private Paint mPaintGray = new Paint();
+    private final int mRectangleSize;
+    private final Paint mPaint = new Paint();
+    private final Paint mPaintWhite = new Paint();
+    private final Paint mPaintGray = new Paint();
 
     private int numRectanglesHorizontal;
     private int numRectanglesVertical;
