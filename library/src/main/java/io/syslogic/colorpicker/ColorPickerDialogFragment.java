3cee238c698f4aba88aa60f6a2034fabc91f4fdf
Martin Zeitler <martin@syslogic.io>
2022-06-13 21:13:41 +0200

    javadoc updated.

--- library/src/main/java/io/syslogic/colorpicker/ColorPickerDialogFragment.java
+++ library/src/main/java/io/syslogic/colorpicker/ColorPickerDialogFragment.java
@@ -24,7 +24,7 @@ import androidx.fragment.app.DialogFragment;
 import io.syslogic.colorpicker.databinding.DialogColorPickerBinding;
 
 /**
- * The Color-Picker {@link DialogFragment}
+ * The Color-Picker {@link DialogFragment} accepts NavArgs and returns a FragmentResult.
  *
  * @author Martin Zeitler
  */
